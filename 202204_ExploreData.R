b35952b8fdca12e3eadb6f2ffb6ac590a170644f
margaretdigiorno <margaret.digiorno@colorado.edu>
2022-05-06 15:34:18 -0600

    Leaflet map radon samples using tmap

--- 202204_ExploreData.R
+++ 202204_ExploreData.R
@@ -8,6 +8,8 @@ library(xts)
 library(data.table)
 library(basemaps)
 library(rgeos)
+library(tmap)
+library(leaflet)
 
 #### Load WQ Data ####
 aprwq <- read.csv('~/columbiariver/field-data/03-processed-data/apr2022_cleandata.csv')
@@ -66,40 +68,55 @@ apr_left_snap <- gInterpolate(leftprof, apr_left$profdist)
 apr_right_snap <- gInterpolate(rightprof, apr_right$profdist)
   
 # Join the data back to the spatial point dataframes of the snapped points and make them into sf objects (for plotting)
-apr_left_snapped <- cbind(apr_left@data, apr_left_snap)
-coordinates(apr_left_snapped) <- ~x+y
-apr_left_snapped <- apr_left_snapped %>% st_as_sf() %>% st_set_crs(epsg3857)
+Left <- cbind(apr_left@data, apr_left_snap)
+coordinates(Left) <- ~x+y
+Left <- Left %>% st_as_sf() %>% st_set_crs(epsg3857)
 
-apr_right_snapped <- cbind(apr_right@data, apr_right_snap)
-coordinates(apr_right_snapped) <- ~x+y
-apr_right_snapped <- apr_right_snapped %>% st_as_sf() %>% st_set_crs(epsg3857)
+Right <- cbind(apr_right@data, apr_right_snap)
+coordinates(Right) <- ~x+y
+Right <- Right %>% st_as_sf() %>% st_set_crs(epsg3857)
+
+# Create marker points to help match profile with map
+leftprof_lab <- gInterpolate(leftprof, seq(0,105000, by = 15000)) %>% st_as_sf() %>% 
+  cbind(seq(0,105000, by = 15000)) %>% rename(marks = seq.0..105000..by...15000.)
+
+rightprof_lab <- gInterpolate(rightprof, seq(0,105000, by = 15000)) %>% st_as_sf() %>% 
+  cbind(seq(0,105000, by = 15000)) %>% rename(marks = seq.0..105000..by...15000.)
+
+leftprof_lab$marks <- paste0(leftprof_lab$marks, ' m')
+rightprof_lab$marks <- paste0(rightprof_lab$marks, ' m')
 
 #### Temp/Cond Profiles ####
 
 # Plot temperature and conductivity vs distance along profile
-for (df in list(apr_left_snapped, apr_right_snapped)){
-  cond <- ggplot(df) + geom_point(aes(x = TIMESTAMP, y = surfcond_corr, color = "Surface")) +
-    geom_point(aes(x = TIMESTAMP, y = deepcond_corr, color = "Deep")) + 
-    labs(title = paste0("April ", d, " 2022"), x="", y ="Conductivity (us/cm)", color = "")+ scale_color_manual(values = c("Surface" = "grey", "Deep" = "black"))
+profiles = list(Left, Right)
+profnames = c('Left', 'Right')
+
+for (i in 1:2){
+  cond <- ggplot(profiles[[i]]) + geom_point(aes(x = profdist, y = surfcond_corr, color = "Surface"), size = 0.2) + 
+    geom_point(aes(x = profdist, y = deepcond_corr, color = "Deep"), size = 0.2) + 
+    labs(title = paste0("April ", profnames[i]), x="", y ="Conductivity (us/cm)", color = "") + 
+    scale_color_manual(values = c("Surface" = "grey", "Deep" = "black")) +
+    lims(y = c(130, 157)) + guides(colour = guide_legend(override.aes = list(size=5)))
   
-  temp <- ggplot(df) + geom_point(aes(x = TIMESTAMP, y = surf_WTemp109_Avg, color = "Surface")) +
-    geom_point(aes(x = TIMESTAMP, y = deep_WTemp109_Avg, color = "Deep")) + 
-    labs(title = paste0("April ", d, " 2022"), x="", y ="Temperature (C)", color = "")+ scale_color_manual(values = c("Surface" = "coral", "Deep" = "red"))
+  temp <- ggplot(profiles[[i]]) + geom_point(aes(x = profdist, y = surf_WTemp109_Avg, color = "Surface"), size = 0.2) + 
+    geom_point(aes(x = profdist, y = deep_WTemp109_Avg, color = "Deep"), size = 0.2) + 
+    labs(title = paste0("April ", profnames[i]), x="", y ="Temperature (C)", color = "") +
+    scale_color_manual(values = c("Surface" = "coral", "Deep" = "red")) + guides(colour = guide_legend(override.aes = list(size=5)))
   
-  ggsave(paste0('202204', df,'_cond.png'), plot = cond, width = 8, height = 2.5, units = 'in')
-  ggsave(paste0('202204', df,'_temp.png'), plot = temp, width = 8, height = 2.5, units = 'in')}
+  ggsave(paste0('202204_', profnames[i],'_cond.png'), plot = cond, width = 8, height = 2.5, units = 'in')
+  ggsave(paste0('202204_', profnames[i],'_temp.png'), plot = temp, width = 8, height = 2.5, units = 'in')}
 
 # Plot temperature and conductivity vs time
-
 for (d in 19:21){
   day <- aprdata[day(aprdata$TIMESTAMP) == d,]
   
-  cond <- ggplot(day) + geom_point(aes(x = TIMESTAMP, y = surfcond_corr, color = "Surface")) +
-    geom_point(aes(x = TIMESTAMP, y = deepcond_corr, color = "Deep")) + 
+  cond <- ggplot(day) + geom_point(aes(x = TIMESTAMP, y = surfcond_corr, color = "Surface"), size = 0.2) +
+    geom_point(aes(x = TIMESTAMP, y = deepcond_corr, color = "Deep"), size = 0.2) + 
     labs(title = paste0("April ", d, " 2022"), x="", y ="Conductivity (us/cm)", color = "")+ scale_color_manual(values = c("Surface" = "grey", "Deep" = "black"))
   
-  temp <- ggplot(day) + geom_point(aes(x = TIMESTAMP, y = surf_WTemp109_Avg, color = "Surface")) +
-    geom_point(aes(x = TIMESTAMP, y = deep_WTemp109_Avg, color = "Deep")) + 
+  temp <- ggplot(day) + geom_point(aes(x = TIMESTAMP, y = surf_WTemp109_Avg, color = "Surface"), size = 0.2) +
+    geom_point(aes(x = TIMESTAMP, y = deep_WTemp109_Avg, color = "Deep"), size = 0.2) + 
     labs(title = paste0("April ", d, " 2022"), x="", y ="Temperature (C)", color = "")+ scale_color_manual(values = c("Surface" = "coral", "Deep" = "red"))
   
   ggsave(paste0('202204', d,'_cond.png'), plot = cond, width = 8, height = 2.5, units = 'in')
@@ -111,6 +128,13 @@ for (d in 19:21){
 # Check that all of the radon data survived (start w/ CR105 and end w/ CR130 - the other samples were for deep holes)
 c(apr_left_snapped[!is.na(apr_left_snapped$Corrected.Mean..pCi.L.), ]$Tape.Color..., apr_right_snapped[!is.na(apr_right_snapped$Corrected.Mean..pCi.L.), ]$Tape.Color...)
 
+# Subset the radon samples
+apr_rn <- filter(apr_sf, !is.na(apr_sf$Corrected.Mean..pCi.L.))
+apr_rn$Corrected.Mean..pCi.L. <- round(apr_rn$Corrected.Mean..pCi.L., digits = 2)
+# Set up tmap
+tmap_mode("view")
+radonmap <- tm_shape(apr_rn) + tm_dots(col = "Corrected.Mean..pCi.L.", id = "Corrected.Mean..pCi.L.", title = "Rn (pCi/L)", size = 0.05)
+tmap_save(radonmap, filename = "202204_Rn.html")
 
 #### Average Data to 10s ####
 # Average data in 10 second chunks for faster plotting
